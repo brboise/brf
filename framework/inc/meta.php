6b1c3b0ad89df73d566972cf5d045de338e4c2fb
brboise <robbymilo@Troys-iMac.local>
2015-10-15 16:12:22 -0600

    Getting organized

---
+++ framework/inc/meta.php
@@ -0,0 +1,3 @@
+<span class="meta-date"><i class="icon-calendar"></i><time datetime="<?php echo date(DATE_W3C); ?>" class="updated"><?php the_time(get_option('date_format')); ?></time></span><span class="meta-category"><i class="icon-align-left"></i><?php the_category(', '); ?></span>
+
+	
